cc6b21a087b01a378dce11663a32077de2cf56e9
Chinh Nguyen <ng.the.chinh@gmail.com>
2021-06-09 16:06:08 +0700

    Add facenet, main and utils

---
+++ main.py
@@ -0,0 +1,147 @@
+import cv2
+import argparse
+import os
+import tensorflow as tf
+from numpy import load
+from statistics import mode, mean
+import threading
+from utils import *
+
+################################################################################
+#######################        GLOBAL SETTINGS        ##########################
+################################################################################
+
+WEIGHT = 'yolo/yolov3-wider_16000.weights'
+MODEL = 'yolo/yolov3-face.cfg'
+
+net = cv2.dnn.readNetFromDarknet(MODEL, WEIGHT)
+net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
+net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
+IMG_SIZE = 416
+CROP_SIZE = 160 # required
+
+# BGR
+BLUE = (255,0,0)
+RED  = (0,0,255)
+
+# KNN
+K_UNKWOWN_THRESHOLD = 9
+K_NB = 5
+MODEL_NAME = 'holy_KNN.h5'
+
+# Init
+thread_finished = True
+total_save = 0
+boxes_buffer = []
+text_buffer = [] 
+
+################################################################################
+##############################     THREAD 2      ###############################
+################################################################################
+
+def detect_and_predict(net, frame, IMG_SIZE, predict_mode, model_facenet):
+    global thread_finished, boxes_buffer, text_buffer
+    thread_finished = False
+    
+    boxes_buffer = []
+    text_buffer = []
+
+    boxes_buffer, confidences = face_detection(net, frame, IMG_SIZE)
+    
+    for box in boxes_buffer:
+        # Extract position data
+        topleft_x, topleft_y, width, height = box[0], box[1], box[2], box[3]
+        
+        # Extract face area
+        face = frame[topleft_y:topleft_y+height, topleft_x:topleft_x+width]
+
+        if predict_mode and (face.size > 0):
+            face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+            resized_face = cv2.resize(face, (CROP_SIZE, CROP_SIZE))
+            # Extract features
+            face_emb = get_embedding(model_facenet, resized_face)
+            face_emb_array = np.asarray(face_emb)
+            face_emb_array = face_emb_array.reshape(1,-1)
+            # Predict
+            text = KNN_predict(knn_model, face_emb_array, trainy, out_encoder, K_UNKWOWN_THRESHOLD)
+        else:
+            text = f'{confidences[i]:.2f}'
+
+        # BUFFER
+        text_buffer.append(text)
+
+    thread_finished = True
+    return thread_finished
+
+
+################################################################################
+#############################     PREPROCESS      ##############################
+################################################################################
+
+# Read arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-o", "--output", type=str, default=None, help="Save face photo to folder")
+args = vars(ap.parse_args())
+
+if args["output"] != None:
+    print("Mode: Collect photo")
+    predict_mode = False
+
+    # Create corresponding folder if not exist
+    if not os.path.exists(os.path.join('data', args["output"])):
+        os.makedirs(os.path.join('data', args["output"]))
+else: 
+    print("Mode: Predict")
+    predict_mode = True
+    
+    # Load photos from folder 'data' and feed to KNN
+    trainX, trainy = load_dataset('data')
+    model_facenet = tf.keras.models.load_model('facenet_keras.h5')
+    newTrainX, trainy, out_encoder = convert_dataset(model_facenet, trainX, trainy)
+    knn_model = KNN_fit(newTrainX, MODEL_NAME, K_NB)
+
+    print(trainX.shape, trainy.shape)
+    print(newTrainX.shape)
+
+################################################################################
+#############################        MAIN        ###############################
+################################################################################
+
+cap = cv2.VideoCapture(0)
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    frame = cv2.flip(frame, 1)
+
+    if thread_finished == True:
+        # Save result
+        boxes_cache = boxes_buffer.copy()
+        text_cache  = text_buffer.copy()
+        x = threading.Thread(target=detect_and_predict, args=(net, frame, IMG_SIZE, predict_mode, model_facenet,)).start()  
+
+    # Draw
+    for i, box in enumerate(boxes_cache):
+        x, y, w, h = box[:4]
+        margin_x, margin_y = 10, 20
+        topleft       = (x-margin_x, y-margin_y)
+        bottomright   = (x+w+margin_x, y+h+margin_y)
+        cv2.rectangle(frame, topleft, bottomright, BLUE, 2)
+        cv2.putText(frame, text_cache[i], topleft, cv2.FONT_HERSHEY_SIMPLEX, 1, RED, 2)
+    
+    cv2.imshow('face detection', frame)
+
+    # Wait for key press
+    key = cv2.waitKey(25) & 0xFF
+    # Capture when only 1 face detected
+    if  predict_mode==False and len(boxes_cache)==1 and key==ord("k"):
+        total_save += 1
+        print(f'Save a photo of {args["output"]}. ID {total_save}')
+        p = os.path.join('data', args["output"], "{}.png".format(str(total_save).zfill(3)))
+        x, y, w, h = boxes_cache[:4]
+        cv2.imwrite(p, frame[y:y+h, x:x+w])
+    elif key == ord("q"):
+        break
+
+print("Camera closed!")
+cap.release()
+cv2.destroyAllWindows()
---
+++ utils.py
@@ -0,0 +1,132 @@
+import os, cv2, pickle, statistics
+import numpy as np
+from sklearn.neighbors import NearestNeighbors
+from sklearn.preprocessing import LabelEncoder
+
+# Load all face images in a directory
+def load_faces(directory, required_size=(160, 160)):
+    faces = list()
+    for filename in os.listdir(directory):
+        path = os.path.join(directory, filename)
+        image = cv2.imread(path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image = cv2.resize(image, required_size)
+        face = np.asarray(image)
+        # store
+        faces.append(face)
+	
+    return faces
+
+# Load data folder containing images of each person
+def load_dataset(directory):
+	X, y = list(), list()
+	# enumerate folders, on per class
+	for subdir in os.listdir(directory):
+		# path
+		path = os.path.join(directory, subdir)
+		# skip any files that might be in the dir
+		if not os.path.isdir(path):
+			continue
+		# load all faces in the subdirectory
+		faces = load_faces(path)
+		# create labels
+		labels = [subdir for _ in range(len(faces))]
+		# summarize progress
+		print('>loaded %d examples for class: %s' % (len(faces), subdir))
+		# store
+		X.extend(faces)
+		y.extend(labels)
+	return np.asarray(X), np.asarray(y)
+
+# Get embedding of 1 face
+def get_embedding(model, face_pixels):
+    # scale pixel values
+    face_pixels = face_pixels.astype('float32')
+    # standardize pixel values across channels (global)
+    mean, std = face_pixels.mean(), face_pixels.std()
+    face_pixels = (face_pixels - mean) / std
+    # transform face into one sample
+    samples = np.expand_dims(face_pixels, axis=0)
+    # make prediction to get embedding
+    yhat = model.predict(samples)
+    return yhat[0]
+
+# Convert each face in the dataset to an embedding
+def convert_dataset(model, trainX, trainy):
+    newTrainX = list()
+    for face_pixels in trainX:
+        embedding = get_embedding(model, face_pixels)
+        newTrainX.append(embedding)
+    newTrainX = np.asarray(newTrainX)
+
+    # label encode targets
+    out_encoder = LabelEncoder()
+    out_encoder.fit(trainy)
+    trainy = out_encoder.transform(trainy)
+    
+    return newTrainX, trainy, out_encoder
+
+def KNN_fit(data, MODEL_NAME, K_NB):
+    if os.path.isfile(MODEL_NAME):
+        model = pickle.load(open(MODEL_NAME, 'rb'))
+    else:
+        model = NearestNeighbors(n_neighbors=K_NB).fit(data)
+        pickle.dump(model, open(MODEL_NAME, 'wb'))
+    return model
+
+def KNN_predict(knn_model, face_emb_array, trainy, out_encoder, K_UNKOWN_THRESHOLD):
+    distances, indices = knn_model.kneighbors(face_emb_array)
+    labels = list()
+    if min(distances[0]) < K_UNKOWN_THRESHOLD:
+        for i in indices[0]:
+            label = trainy[i]
+            labels.extend([label])
+        try:
+            prediction = statistics.mode(labels)
+            predict_name = out_encoder.inverse_transform([prediction])
+        # no unique mode; found 2 equally common values
+        except statistics.StatisticsError:
+            predict_name = "Unknown"
+    else:
+        predict_name = "Unknown"
+    
+    text = f'{predict_name}, {min(distances[0]):.2f}'
+    return text
+
+def face_detection(net, frame, IMG_SIZE):
+    blob = cv2.dnn.blobFromImage(frame, 1/255, (IMG_SIZE, IMG_SIZE),
+                                [0, 0, 0], 1, crop=False)
+
+    net.setInput(blob)
+    output_layers = net.getUnconnectedOutLayersNames() # take 3 output layers
+    outs = net.forward(output_layers)
+
+    frame_height = frame.shape[0]
+    frame_width = frame.shape[1]
+
+    confidences = []
+    boxes = []
+
+    # Each frame produces 3 outs corresponding to 3 output layers
+    for out in outs:
+        # 1 out has multiple predictions with length of 6
+        for detection in out:
+            confidence = detection[-1]
+            # Extract position data of face area (only area with high confidence)
+            if confidence > 0.5:
+                center_x = int(detection[0] * frame_width)
+                center_y = int(detection[1] * frame_height)
+                width    = int(detection[2] * frame_width)
+                height   = int(detection[3] * frame_height)
+                
+                # Find the top left point of the bounding box 
+                topleft_x = center_x - width//2 
+                topleft_y = center_y - height//2
+                confidences.append(float(confidence))
+                boxes.append([topleft_x, topleft_y, width, height])
+
+    # Perform non maximum suppression to eliminate redundant
+    # overlapping boxes with lower confidences.
+    indices = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+    final_boxes = [boxes[i[0]] for i in indices]
+    return final_boxes, confidences
